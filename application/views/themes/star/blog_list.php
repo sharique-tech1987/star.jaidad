cde0805ccd08884f935a65f6fde2032a02ea762f
umair_petrolsolution <umair.fetchsky@gmail.com>
2019-10-29 17:12:07 +0500

    Blog Page ADDED

---
+++ application/views/themes/star/blog_list.php
@@ -0,0 +1,125 @@
+<div id="primary-content" class="pd-top-100 pd-bottom-100 archive-wrap archive-large-image">
+    <div class="container clearfix">
+        <div class="row">
+            <div class="col-md-9 archive-inner">
+                <div class="blog-wrap clearfix">
+                    <article id="post-3523" class="post-large-image clearfix post-3523 post type-post status-publish format-standard has-post-thumbnail hentry category-apartment category-real-estates tag-apartment tag-villa">
+                        <div class="entry-content-wrap clearfix">
+                            <div class="entry-thumb-wrap">
+                                <div class="entry-thumbnail">
+                                    <a href="#" title="We are Offering the Best Real Estate Deals" class="entry-thumbnail-overlay">
+                                        <img src="http://themes.g5plus.net/benaa/wp-content/uploads/2018/01/properties-1.jpg" alt="We are Offering the Best Real Estate Deals" class="img-responsive"> </a>
+                                    <a data-thumb-src="http://themes.g5plus.net/benaa/wp-content/uploads/2018/01/properties-1-340x340.jpg" data-gallery-id="1701687358" data-rel="lightGallery" href="http://themes.g5plus.net/benaa/wp-content/uploads/2018/01/properties-1.jpg" class="zoomGallery">
+                                        <i class="fa fa-expand"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="entry-post-meta">
+                                <div class="entry-meta-author">
+                                    <span>By </span><a href="#">admin</a>
+                                </div>
+                                <div class="entry-meta-date">
+                                    <a href="#">January 16, 2018</a>
+                                </div>
+                            </div>
+                            <div class="entry-content-inner">
+                                <div class="entry-info-post clearfix">
+                                    <h4 class="entry-post-title">
+                                        <a title="We are Offering the Best Real Estate Deals" href="#">We are Offering the Best Real Estate Deals</a></h4>
+                                    <div class="entry-excerpt"> Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo [...]
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                    <div class="paging-navigation clearfix">
+                        <span aria-current="page" class="page-numbers current">1</span>
+                        <a class="page-numbers" href="#">2</a>
+                        <a class="next page-numbers" href="#">Next</a>
+                    </div>
+                </div>
+            </div>
+            <div class="sidebar-mobile-canvas-icon" title="Click to show Canvas Sidebar">
+                <i class="fa fa-sliders"></i>
+            </div>
+            <div class="wrapper-sticky" style="display: block; height: 1134px; width: 25%; margin: auto; position: relative; float: left; left: auto; right: auto; top: auto; bottom: auto; vertical-align: top;">
+                <div class="primary-sidebar sidebar col-md-3 sidebar-mobile-canvas gf-sticky" style="left: 0px; width: 300px; position: absolute; top: 0px;">
+                    <aside id="search-2" class="widget widget_search">
+                        <h4 class="widget-title"><span>Search</span></h4>
+                        <form role="search" class="search-form" method="get" id="searchform" action="http://themes.g5plus.net/benaa/">
+                            <input type="text" value="" name="s" id="s" placeholder="ENTER YOUR  KEYWORD">
+                            <button type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </form>
+                    </aside>
+                    <aside id="recent-posts-2" class="widget widget_recent_entries">
+                        <h4 class="widget-title">
+                            <span>Recent Posts</span>
+                        </h4>
+                        <ul>
+                            <li>
+                                <a href="#">We are Offering the Best Real Estate Deals</a>
+                            </li>
+                            <li>
+                                <a href="#">Recent Trends in Story Telling 2017</a>
+                            </li>
+                            <li>
+                                <a href="#">Construction You Can Count On</a>
+                            </li>
+                            <li>
+                                <a href="#">Project Luxury Villa in Rego Park</a>
+                            </li>
+                            <li>
+                                <a href="#">Boutique Space Greenville at 2017</a>
+                            </li>
+                        </ul>
+                    </aside>
+                    <aside id="categories-2" class="widget widget_categories">
+                        <h4 class="widget-title">
+                            <span>Categories</span>
+                        </h4>
+                        <ul>
+                            <li class="cat-item cat-item-2">
+                                <a href="#">Apartment (6)</a>
+                            </li>
+                            <li class="cat-item cat-item-3">
+                                <a href="#">Clients (1)</a>
+                            </li>
+                            <li class="cat-item cat-item-4">
+                                <a href="#">Green (3)</a>
+                            </li>
+                            <li class="cat-item cat-item-5">
+                                <a href="#">Mountain (1)</a>
+                            </li>
+                            <li class="cat-item cat-item-6">
+                                <a href="#">Real Estates (7)</a>
+                            </li>
+                            <li class="cat-item cat-item-7">
+                                <a href="#">Villa (7)</a>
+                            </li>
+                        </ul>
+                    </aside>
+                    <aside id="tag_cloud-2" class="widget widget_tag_cloud">
+                        <h4 class="widget-title"><span>Tags</span></h4>
+                        <div class="tagcloud">
+                            <a href="#" class="tag-cloud-link tag-link-8 tag-link-position-1" style="font-size: 8pt;" aria-label="agency (1 item)">agency</a>
+                            <a href="#" class="tag-cloud-link tag-link-9 tag-link-position-2" style="font-size: 8pt;" aria-label="agent (1 item)">agent</a>
+                            <a href="#" class="tag-cloud-link tag-link-10 tag-link-position-3" style="font-size: 8pt;" aria-label="Apartment (1 item)">Apartment</a>
+                            <a href="#" class="tag-cloud-link tag-link-11 tag-link-position-4" style="font-size: 18.181818181818pt;" aria-label="Building (4 items)">Building</a>
+                            <a href="#" class="tag-cloud-link tag-link-12 tag-link-position-5" style="font-size: 22pt;" aria-label="Building Construction (6 items)">Building Construction</a>
+                            <a href="#" class="tag-cloud-link tag-link-13 tag-link-position-6" style="font-size: 18.181818181818pt;" aria-label="Construction (4 items)">Construction</a>
+                            <a href="#" class="tag-cloud-link tag-link-14 tag-link-position-7" style="font-size: 8pt;" aria-label="Home (1 item)">Home</a>
+                            <a href="#" class="tag-cloud-link tag-link-15 tag-link-position-8" style="font-size: 12.581818181818pt;" aria-label="homes (2 items)">homes</a>
+                            <a href="#" class="tag-cloud-link tag-link-16 tag-link-position-9" style="font-size: 8pt;" aria-label="house (1 item)">house</a>
+                            <a href="#" class="tag-cloud-link tag-link-17 tag-link-position-10" style="font-size: 8pt;" aria-label="listing (1 item)">listing</a>
+                            <a href="#" class="tag-cloud-link tag-link-18 tag-link-position-11" style="font-size: 8pt;" aria-label="property (1 item)">property</a>
+                            <a href="#" class="tag-cloud-link tag-link-19 tag-link-position-12" style="font-size: 8pt;" aria-label="realtor (1 item)">realtor</a>
+                            <a href="#" class="tag-cloud-link tag-link-20 tag-link-position-13" style="font-size: 18.181818181818pt;" aria-label="villa (4 items)">villa</a>
+                        </div>
+                    </aside>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
